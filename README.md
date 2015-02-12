1da32aa57db1ef29b3e5109a28463389cf84b217
Bryant Syme <prefu@hotmail.com>
2015-02-11 23:24:59 -0800

    added first draft of runner/watcher

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # guardian-runner
-Node console runner for guardianjs micro testing framework.
+Node console runner for guardianjs micro testing framework.  However you structure
+
+## Install
+
+```bash
+$ npm install guardian-runner
+```
+
+## Usage
+
+```bash
+$ node_modules/guardian-runner start
+```
+
+By default this will run test.js and watch for file changes with a default matching the glob pattern of '*.js' (except node_modules folder) which can be overridden with the following:
+
+```bash
+$ node_modules/guardian-runner start [testfile] [globpatterns]...
+```
--- index.js
+++ index.js
@@ -1,3 +1,39 @@
 #!/usr/bin/env node
 
-console.log('test');
+function runner(file, patterns) {
+	'use strict';
+
+	var spawn = require('child_process').fork;
+	var gaze = require('gaze');
+	var proc;
+
+	var ignoreForFasterWatch = '!**/node_modules/**';
+	var matches = [ignoreForFasterWatch].concat(patterns.length ? patterns : ['*.js']);
+
+	function spawnProc() {
+		if (proc) {
+			proc.kill('SIGHUP');
+		}
+		proc = spawn(file || 'test.js');
+	}
+
+	spawnProc();
+
+	gaze(matches, function () {
+		this.on('all', function (type, file) {
+			spawnProc();
+		});
+	});
+}
+
+if (process.argv.length) {
+	if (process.argv.indexOf('start') > -1) {
+		runner(process.argv[3], process.argv.splice(4));
+	} else {
+		console.log('Usage:  node_modules/guardian-runner start [file] [pattern]...');
+		console.log('Default:  node_modules/guardian-runner start test.js \'.*js\'');
+		process.exit();
+	}
+}
+
+module.exports = runner;
